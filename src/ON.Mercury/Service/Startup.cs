78e776e33b728ddb357080b204be44656c0a0094
Andrew Mingst <amingst@timcast.com>
2023-08-15 00:24:00 -0400

    Event  Service grpc

--- src/ON.Mercury/Service/Startup.cs
+++ src/ON.Mercury/Service/Startup.cs
@@ -41,6 +41,7 @@ public class Startup
             endpoints.MapGrpcService<ChannelService>();
             endpoints.MapGrpcService<RoleService>();
             endpoints.MapGrpcService<MemberService>();
+            endpoints.MapGrpcService<EventService>();
         });
     }
 }
