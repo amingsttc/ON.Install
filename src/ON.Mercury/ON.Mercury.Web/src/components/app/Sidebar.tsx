9b69dac47ac36ca087907beae45407fe5bc92b30
amingsttc <amingst@timcast.com>
2023-08-30 21:08:16 -0400

    Fix username break

--- src/ON.Mercury/ON.Mercury.Web/src/components/app/Sidebar.tsx
+++ src/ON.Mercury/ON.Mercury.Web/src/components/app/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { For, createEffect } from 'solid-js';
+import { For, createMemo } from 'solid-js';
 import './Sidebar.scss';
 import { SidebarChannel } from '../channels/SidebarChannel';
 import { Link } from '@solidjs/router';
@@ -8,26 +8,22 @@ import CustomBulletItem from '../decorations/bullet/CustomBulletItem';
 import profileDefaultImage from '../../assets/profile_default.png';
 export function Sidebar() {
 	const { channels, currentMember } = useGlobalContext();
-	let username = '';
 
-	createEffect(() => {
-		if (currentMember()) {
-			return () => {
-				username = currentMember().username;
-			};
-		}
-	}, username);
+	const memoUsername = createMemo(() => {
+		return currentMember() ? currentMember().username : 'username';
+	});
 
-	// TODO: fix username break
 	return (
-		<div class='sidebar'>
-			<div class='sidebar-header'>
+		<div class="sidebar">
+			<div class="sidebar-header">
 				<h3>ServerName</h3>
 			</div>
-			<div class='container'>
-				<ul class='list'>
-					<Link href='/' class='sidebar-link sidebar-channel'>
-						<CustomBulletItem bullet='#'>Directory</CustomBulletItem>
+			<div class="container">
+				<ul class="list">
+					<Link href="/" class="sidebar-link sidebar-channel">
+						<CustomBulletItem bullet="#">
+							Directory
+						</CustomBulletItem>
 					</Link>
 					<For each={channels()}>
 						{(channel) => {
@@ -41,12 +37,12 @@ export function Sidebar() {
 					</For>
 				</ul>
 			</div>
-			<div class='sidebar-footer username-container'>
-				<div class='message-avatar'>
-					<img src={profileDefaultImage} alt='Profile' />
+			<div class="sidebar-footer username-container">
+				<div class="message-avatar">
+					<img src={profileDefaultImage} alt="Profile" />
 				</div>
 				<div>
-					<h3>@{username || 'username'}</h3>
+					<h3>@{memoUsername()}</h3>
 				</div>
 			</div>
 		</div>
